a474116ebc3321dd1983bcabfd2bedc433016244
Jack Pettigrew <jak.pettigrew@gmail.com>
2021-03-09 17:22:12 +0000

    Added SpinNode to replace DeleteSequence

--- Assets/Scripts/AI/Controller/AIBeahviourTreeController.cs
+++ Assets/Scripts/AI/Controller/AIBeahviourTreeController.cs
@@ -25,10 +25,11 @@ namespace DD.AI.Controllers
             Sequence followSequence = new Sequence(behaviourTree, new List<Node> { new MoveToPlayerNode(behaviourTree, this), new JumpNode(behaviourTree,this) });
 
             CanSeePlayerNode canSeePlayerNode = new CanSeePlayerNode(behaviourTree,transform, 45.0f, 2.0f, playerLayerMask);
-            DeleteSelfNode deleteSelfNode = new DeleteSelfNode(behaviourTree,transform);
-            Sequence deleteSelfSequence = new Sequence(behaviourTree, new List<Node> { canSeePlayerNode, deleteSelfNode });
+            //DeleteSelfNode deleteSelfNode = new DeleteSelfNode(behaviourTree,transform);
+            SpinNode spinNode = new SpinNode(behaviourTree, transform);
+            Sequence spinSelfSequence = new Sequence(behaviourTree, new List<Node> { canSeePlayerNode, spinNode });
 
-            Selector root = new Selector(behaviourTree,new List<Node> { deleteSelfSequence, followSequence });
+            Selector root = new Selector(behaviourTree,new List<Node> { spinSelfSequence, followSequence });
 
             behaviourTree.SetBehaviourTree(root);
         }
