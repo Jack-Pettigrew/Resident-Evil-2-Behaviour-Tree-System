9beb0f732bfac4ffbfbafa5d0035b994cac4448d
Jack Pettigrew <jak.pettigrew@gmail.com>
2021-12-02 20:23:37 +0000

    Composite Node: Added Service node list
    Composite nodes can evaluate service nodes before they evaluate their children. Allows for service nodes to be treated normal child nodes of composites or run as prerequisites to the current composite.

--- Assets/Scripts/AI/Behaviour Tree/Nodes/Composite/Composite.cs
+++ Assets/Scripts/AI/Behaviour Tree/Nodes/Composite/Composite.cs
@@ -11,10 +11,37 @@ namespace DD.AI.BehaviourTreeSystem
         /// </summary>
         protected List<Node> childNodes;
 
+        /// <summary>
+        /// List of Service nodes to be executed before Composite evaluation.
+        /// </summary>
+        protected List<Service> serviceNodes;
 
         public Composite(BehaviourTree behaviourTree, List<Node> childNodes) : base(behaviourTree)
         {
             this.childNodes = childNodes;
+            this.serviceNodes = null;
+        }
+
+        public Composite(BehaviourTree behaviourTree, List<Node> childNodes, List<Service> serviceNodes) : base(behaviourTree)
+        {
+            this.childNodes = new List<Node>(childNodes);
+            this.serviceNodes = serviceNodes;
+        }
+
+        protected override bool OnStart()
+        {
+            if(serviceNodes?.Count > 0)
+            {
+                foreach (var node in serviceNodes)
+                {
+                    if(node.UpdateNode() == NodeState.FAILED)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
