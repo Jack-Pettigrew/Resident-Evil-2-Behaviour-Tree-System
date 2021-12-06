c1537e4cb142e168d2d28c912ca5cddf5a33067c
Jack Pettigrew <jak.pettigrew@gmail.com>
2021-12-06 20:59:45 +0000

    IsInSameRoom Conditional

---
+++ Assets/Scripts/AI/Behaviour Tree/Nodes/Conditional Nodes/IsInSameRoomAs.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DD.Systems.Room;
+
+namespace DD.AI.BehaviourTreeSystem
+{
+    public class IsInSameRoomAs<T> : Conditional where T : Component
+    {
+        private readonly string targetBlackboardKey;
+
+        public IsInSameRoomAs(BehaviourTree behaviourTree, string targetBlackboardKey) : base(behaviourTree)
+        {
+            this.targetBlackboardKey = targetBlackboardKey;
+        }
+
+        protected override NodeState EvaluateConditional()
+        {
+            return RoomManager.IsObjectInRoom(
+                behaviourTree.ai.GetAITransform().gameObject,
+                RoomManager.GetRoomOfObject(behaviourTree.Blackboard.GetFromBlackboard<T>(targetBlackboardKey).gameObject)
+            ) ? NodeState.SUCCESSFUL : NodeState.FAILED;
+
+        }
+    }
+}
