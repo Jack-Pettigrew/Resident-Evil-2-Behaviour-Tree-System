590b06b45730cfd2ff783cba6f57897f2a27c187
Jack Pettigrew <jak.pettigrew@gmail.com>
2021-12-27 15:07:14 +0000

    Rename GetDoorFromPath + Door path null check

---
+++ Assets/Scripts/AI/Behaviour Tree/Nodes/Service/GetDoorFromPath.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DD.Systems.Room;
+
+namespace DD.AI.BehaviourTreeSystem
+{
+    public class GetDoorFromPath : UpdateBlackboardService
+    {
+        private readonly string doorPathBlackboardKey;
+        private readonly string doorPathIndexBlackboardKey;
+        private readonly string targetBlackboardKey;
+
+        public GetDoorFromPath(BehaviourTree behaviourTree, string doorPathBlackboardKey, string doorPathIndexBlackboardKey, string targetBlackboardKey) : base(behaviourTree)
+        {
+            this.doorPathBlackboardKey = doorPathBlackboardKey;
+            this.doorPathIndexBlackboardKey = doorPathIndexBlackboardKey;
+            this.targetBlackboardKey = targetBlackboardKey;
+        }
+
+        protected override bool UpdateBlackboard()
+        {
+            if (!behaviourTree.Blackboard.IsBlackboardVariableNull(doorPathBlackboardKey))
+            {
+                return behaviourTree.Blackboard.UpdateBlackboardVariable(targetBlackboardKey,
+                    behaviourTree.Blackboard.GetFromBlackboard<Door[]>(doorPathBlackboardKey)[
+                        behaviourTree.Blackboard.GetFromBlackboard<int>(doorPathIndexBlackboardKey)
+                    ]
+                );
+            }
+
+            return false;
+        }
+    }
+}
