b0a7f520214378d155ad82a8039190c5dcf6d290
Jack Pettigrew <jak.pettigrew@gmail.com>
2022-01-01 22:40:39 +0000

    IInteruptable Interface
    Used to provide additional logic for Node that can be interupted

---
+++ Assets/Scripts/AI/Behaviour Tree/Interfaces/IInteruptable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DD.AI.BehaviourTreeSystem
+{
+    public interface IInteruptable
+    {
+        void Interupt();
+    }
+}
