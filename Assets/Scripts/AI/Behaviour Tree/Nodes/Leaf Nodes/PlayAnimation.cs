333cd9c543df84c84eb2a5fa51fa32ece20886f5
Jack Pettigrew <jak.pettigrew@gmail.com>
2022-01-31 22:13:36 +0000

    PlayAnimation BT Node

--- Assets/Scripts/AI/Behaviour Tree/Nodes/Leaf Nodes/PlayAnimation.cs
+++ Assets/Scripts/AI/Behaviour Tree/Nodes/Leaf Nodes/PlayAnimation.cs
@@ -8,17 +8,44 @@ namespace DD.AI.BehaviourTreeSystem
     public class PlayAnimation : Node
     {
         private readonly string stateName;
+        private bool waitForAnimationToComplete;
+        private bool waitingForAnimation = false;
+        private bool waitComplete = true;
 
-        public PlayAnimation(BehaviourTree behaviourTree, string stateName) : base(behaviourTree)
+        public PlayAnimation(BehaviourTree behaviourTree, string stateName, bool waitForAnimationToComplete = false) : base(behaviourTree)
         {
             this.stateName = stateName;
+            this.waitForAnimationToComplete = waitForAnimationToComplete;
         }
 
         protected override NodeState Evaluate()
         {
-            behaviourTree.ai.GetAnimator().Play(stateName);
+            if (waitForAnimationToComplete)
+            {
+                if (waitingForAnimation)
+                {
+                    if (waitComplete)
+                    {
+                        waitingForAnimation = false;
+                        return NodeState.SUCCESSFUL;
+                    }
+
+                    return NodeState.RUNNING;
+                }
+
+                behaviourTree.ai.GetAnimator().PlayAndWait(stateName, StopWaiting);
+                waitingForAnimation = true;
+                waitComplete = false;
+                return NodeState.RUNNING;
+            }
+
+            behaviourTree.ai.GetAnimator().Animator.SetTrigger(stateName);
             return NodeState.SUCCESSFUL;
         }
-    }
 
+        private void StopWaiting()
+        {
+            waitComplete = true;
+        }
+    }
 }
