9a081efc7a5bc32cdcb434fff71be874b208617b
Jack Pettigrew <jak.pettigrew@gmail.com>
2022-02-02 22:20:30 +0000

    Health: Heal method + more events!

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -9,21 +9,32 @@ namespace DD.Core
         [field: Header("Health")]
         [field: SerializeField] public bool IsDamagable { get; set; }
 
-        [SerializeField] private float maxHealth = 100.0f;
+        [SerializeField, Min(0)] private float maxHealth = 100.0f;
         public float CurrentHealth { get; private set; }
+        public bool IsDead { get; private set; }
 
         [Space(10)]
         [SerializeField] protected bool hasDamageCooldown = true;
         [SerializeField] protected float damageCooldown = 1.0f;
 
-        public bool IsDead { get; private set; }
+        [Space(10)]
+
+        public UnityEvent<float> OnHealthHealed;
+        public UnityEvent<float> OnDamageTaken;
         public UnityEvent OnDeath;
+
+        public void Heal(float healAmount)
+        {
+            CurrentHealth = Mathf.Min(CurrentHealth += healAmount, maxHealth);
+            OnHealthHealed?.Invoke(healAmount);
+        }
 
         public virtual void TakeDamage(float damageAmount)
         {
             if (!IsDamagable || IsDead) return;
 
             CurrentHealth -= damageAmount;
+            OnDamageTaken?.Invoke(damageAmount);
 
             if (CurrentHealth <= 0)
             {
