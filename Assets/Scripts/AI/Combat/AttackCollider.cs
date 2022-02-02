03d6243f99e61ce9a3c53cec3d8bb6e7a65a48f0
Jack Pettigrew <jak.pettigrew@gmail.com>
2022-02-02 21:59:53 +0000

    Namespace updating

--- Assets/Scripts/AI/Combat/AttackCollider.cs
+++ Assets/Scripts/AI/Combat/AttackCollider.cs
@@ -2,34 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackCollider : MonoBehaviour
+namespace DD.Core.Combat
 {
-    [Header("Components")]
-    [SerializeField] private Collider attackCollider;
-    [Header("Damage")]
-    [Tooltip("The amount of damage an object will recieve.")]
-    [SerializeField] private int damageAmount;
-    
-    private void Awake() {
-        attackCollider.enabled = false;
+    public class AttackCollider : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private Collider attackCollider;
+        [Header("Damage")]
+        [Tooltip("The amount of damage an object will recieve.")]
+        [SerializeField] private int damageAmount;
 
-        if(attackCollider == null)
-        { 
-            Debug.LogWarning($"{gameObject.name} attack collider has no reference to a Collider!");
+        private void Awake()
+        {
+            attackCollider.enabled = false;
+
+            if (attackCollider == null)
+            {
+                Debug.LogWarning($"{gameObject.name} attack collider has no reference to a Collider!");
+            }
         }
-    }
-    
-    public void EnableCollider()
-    {
-        attackCollider.enabled = true;
-    }
 
-    public void DisableCollider()
-    {
-        attackCollider.enabled = false;
-    }
+        public void EnableCollider()
+        {
+            attackCollider.enabled = true;
+        }
 
-    private void OnCollisionEnter(Collision other) {
-        other.gameObject.GetComponent<IDamagable>().TakeDamage(damageAmount);
+        public void DisableCollider()
+        {
+            attackCollider.enabled = false;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            other.gameObject.GetComponent<IDamagable>().TakeDamage(damageAmount);
+        }
     }
 }
--- Assets/Scripts/Editor/AttackColliderInspector.cs
+++ Assets/Scripts/Editor/AttackColliderInspector.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEditor;
+using DD.Core.Combat;
 
 [CustomEditor(typeof(AttackCollider))]
 public class AttackColliderInspector : Editor {
