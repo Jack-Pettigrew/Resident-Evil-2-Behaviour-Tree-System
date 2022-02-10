c59a1c64bfdc785478c43608142540e1e639a733
Jack Pettigrew <jak.pettigrew@gmail.com>
2022-02-10 19:28:15 +0000

    ItemData class
    Holds information about an item

---
+++ Assets/Scripts/Items/ItemData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DD.Core.Items
+{
+    [CreateAssetMenu(fileName = "ItemData", menuName = "Items/ItemData")]
+    public class ItemData : ScriptableObject
+    {
+        [Header("Base Info")]
+        public string itemName;
+        [TextArea] public string itemDescription;
+        public GameObject itemPrefab;
+        public Sprite itemIcon;
+
+        [Header("Inventory Settings")]
+        public bool isStackable = true;
+        public int maxStackSize = 10;        
+    }
+}
