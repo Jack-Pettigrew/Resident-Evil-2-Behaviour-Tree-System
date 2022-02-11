d62711bdc85d2104ea0df1dc5eb9796ca82bf985
Jack Pettigrew <jak.pettigrew@gmail.com>
2022-02-11 19:14:36 +0000

    ItemInteractable
    - Currently adds item to inventory

---
+++ Assets/Scripts/Interactables/ItemInteractable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DD.Core.Items;
+using DD.Core.InventorySystem;
+
+namespace DD.Core
+{
+    public class ItemInteractable : Interactable
+    {
+        [SerializeField] private ItemData itemData;
+        
+        public override void Interact()
+        {
+            Inventory.Instance.AddItem(itemData);
+            Destroy(gameObject);
+        }
+    }
+}
