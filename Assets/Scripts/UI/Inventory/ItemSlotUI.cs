ef80ae347f79627206e4531fa64e6ac7ceb9f423
Jack-Pettigrew <jak.pettigrew@gmail.com>
2022-03-20 19:59:56 +0000

    MILESTONE: ItemSlotUI works with ContextMenuUI

--- Assets/Scripts/UI/Inventory/ItemSlotUI.cs
+++ Assets/Scripts/UI/Inventory/ItemSlotUI.cs
@@ -17,6 +17,14 @@ namespace DD.UI
         [SerializeField] private GameObject itemToggle;
         [SerializeField] private TextMeshProUGUI amountText;
         [SerializeField] private RawImage itemImage;
+
+        private ContextMenuUI contextMenu;
+
+        private void Awake()
+        {
+            contextMenu = FindObjectOfType<ContextMenuUI>();
+        }
+
         /// <summary>
         /// Sets up the link between this UI slot the given ItemSlot. 
         /// </summary>
@@ -47,7 +55,19 @@ namespace DD.UI
 
         public override void Select()
         {
-            Debug.LogError($"ItemSlotUI {name} has no Select method logic");
+            // Cancel if no Item
+            if(ItemSlot == null)
+            {
+                return;
+            }
+            
+            contextMenu.SetContextMenu(new ContextMenuOption[] {
+                new ContextMenuOption("Use", ItemSlot.ItemData.Use),
+                new ContextMenuOption("Drop", () => {Inventory.Instance.DropItem(ItemSlot.ItemData);})
+            });
+
+            contextMenu.SetPosition(transform.position);
+            contextMenu.ShowContextMenu();
         }
     }
 }
