30a3ed32952b9a1be31b5beae10941638b827bc4
Jack-Pettigrew <jak.pettigrew@gmail.com>
2022-04-26 10:37:07 +0100

    ReadOnly CustomAttribute

---
+++ Assets/Scripts/Custom Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+public class ReadOnlyAttribute : PropertyAttribute
+{
+    
+}
