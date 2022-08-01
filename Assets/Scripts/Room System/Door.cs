00070cf733a12c8d45e6402ae3f03a00a6d55ac5
Jack-Pettigrew <jak.pettigrew@gmail.com>
2022-08-01 19:06:46 +0100

    Door manual open & closing + wait until finished

--- Assets/Scripts/Room System/Door.cs
+++ Assets/Scripts/Room System/Door.cs
@@ -13,6 +13,7 @@ namespace DD.Systems.Room
     {
         // STATE
         public bool CanInteract { set; get; }
+        public bool IsChangingState { private set; get; }
         public bool IsOpen { private set; get; }
         public bool IsLocked { private set; get; }
 
@@ -21,17 +22,22 @@ namespace DD.Systems.Room
         public float doorSpeed = 3.0f;
         [SerializeField] private float openDoorCooldown = 5.0f;
         private float closedTargetRotation;
+        [SerializeField, Range(0, 0.5f), Tooltip("The threshold of which the door is considered closed during it's angular Lerp.")]
+        private float doorClosedThreshold = 0.1f;
+
+        // Coroutine
+        private Coroutine runningCoroutine;
 
         // Components
-        [SerializeField] private new Rigidbody rigidbody;
+        [SerializeField] private Transform hingeParentTransform;
+        [Header("Components"), SerializeField] private new Rigidbody rigidbody;
 
         // ROOMS
-        [Header("Connecting Room A")]
+        [Header("Connecting Rooms")]
         [SerializeField] private Room roomA;
         public Room RoomA { get { return roomA; } }
         [Tooltip("The point where characters can start entering the Door from.")] public Transform roomAEntryPoint;
-
-        [Header("Connecting Room B")]
+        [Space(10)]
         [SerializeField] private Room roomB;
         public Room RoomB { get { return roomB; } }
         [Tooltip("The point where characters can start entering the Door from.")] public Transform roomBEntryPoint;
@@ -43,10 +49,15 @@ namespace DD.Systems.Room
 
         private void Awake() {
             CanInteract = true;
-
+            
             if(IsLocked)
             {
                 ignorePlayerCollision = true;
+            }
+
+            if(!hingeParentTransform)
+            {
+                Debug.LogWarning($"Door '{name}' does not have a hinge parent assigned. Please assign a hinge parent to this Door.");
             }
 
             if(!rigidbody)
@@ -64,9 +75,15 @@ namespace DD.Systems.Room
         /// </summary>
         public void Interact(Interactor interactor)
         {
-            if(IsLocked) 
+            if(IsLocked || IsChangingState) 
             {
                 return;
+            }
+            
+            if(runningCoroutine != null)
+            {
+                StopCoroutine(runningCoroutine);
+                runningCoroutine = null;
             }
             
             if(!IsOpen)
@@ -89,7 +106,7 @@ namespace DD.Systems.Room
         - Dynamic physic move - player only
             - while in collision with player, allow Y axis rotation and physics forces to push door up to limit
             - like above "start close cooldown if door is open and player is no longer touching it"
-        - STOP COROUTINES FROM OVERLAPPING (e.g. closing door multiple times will stack coroutines)
+        - STOP COROUTINES FROM OVERLAPPING (e.g. closing door multiple times will stack coroutines) **********************
         */
         
 
@@ -103,18 +120,14 @@ namespace DD.Systems.Room
             // Determine target rotation
             targetAngle = Mathf.Repeat(targetAngle, 360);
 
-            Debug.LogWarning(targetAngle);
+            // Debug.LogWarning(-targetAngle);
 
             // Wait until door has rotated to target angle
-            while(Mathf.Abs(Mathf.DeltaAngle(transform.localEulerAngles.y, targetAngle)) > 0.5f)
+            while(Mathf.Abs(Mathf.DeltaAngle(hingeParentTransform.localEulerAngles.y, targetAngle)) > doorClosedThreshold)
             {
                 yield return new WaitForFixedUpdate();
-                // yield return null;
 
-                transform.localEulerAngles = new Vector3(0, targetAngle, 0);
-
-                // FIX THIS STUPID CODE BELOW IT DONT WORK THE ABOVE DOES
-                // rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * Mathf.LerpAngle(transform.eulerAngles.y, targetAngle, doorSpeed * Time.deltaTime)));
+                hingeParentTransform.localEulerAngles = Vector3.up * Mathf.LerpAngle(hingeParentTransform.localEulerAngles.y, targetAngle, Time.deltaTime * doorSpeed);
             }
         }
 
@@ -124,40 +137,42 @@ namespace DD.Systems.Room
         /// <returns></returns>
         public void OpenDoor(Vector3 openerPosition)
         {
-            StartCoroutine(ManuallyOpenDoor(openerPosition));
+            runningCoroutine = StartCoroutine(ManuallyOpenDoor(openerPosition));
         }
 
         private IEnumerator ManuallyOpenDoor(Vector3 openerPosition)
         {            
             IsOpen = true;
+            IsChangingState = true;
             // TODO: Play Door opening noise
             
             ignorePlayerCollision = true;
             // TODO: ignore player collisions
             
-            Debug.Log("Openning Door...");
+            // Debug.Log("Openning Door...");
                         
             // TODO: base open offset on interactor position and pass to function
 
-            Debug.LogWarning(Vector3.Dot(transform.forward, (openerPosition - transform.position).normalized));
+            // Debug.LogWarning(Vector3.Dot(transform.forward, (openerPosition - transform.position).normalized));
 
+            // Rotate door towards the offset (-90 = +Z | 90 = -Z)
             yield return SlerpDoorToOffset(
                 Vector3.Dot(transform.forward, (openerPosition - transform.position).normalized) < 0 ? 90.0f : -90.0f
             );
 
-            Debug.Log("Door fully open!");
+            // Debug.Log("Door fully open!");
 
+            IsChangingState = false;
             openDoorEvent?.Invoke();
 
-            // TODO: start cooldown coroutine
-            StartCoroutine(OpenDoorCooldown());
+            runningCoroutine = StartCoroutine(OpenDoorCooldown());
         }
 
         private IEnumerator OpenDoorCooldown()
         {
             float timer = openDoorCooldown;
 
-            Debug.Log("Door cooldown...");
+            // Debug.Log("Door cooldown...");
 
             while (timer > 0)
             {
@@ -166,18 +181,21 @@ namespace DD.Systems.Room
             }
 
             // TODO: start door close dynamic
-            StartCoroutine(CloseDoorDynamic());
+            runningCoroutine = StartCoroutine(CloseDoorDynamic());
         }
 
         private IEnumerator CloseDoorDynamic()
         {
-            Debug.Log("Closing Door...");
+            // Debug.Log("Closing Door...");
+
+            IsChangingState = true;
             
             yield return SlerpDoorToOffset(0.0f);
             
-            Debug.Log("Door closed!");
+            // Debug.Log("Door closed!");
 
             IsOpen = false;
+            IsChangingState = false;
             closeDoorEvent?.Invoke();
         }
 
@@ -187,7 +205,7 @@ namespace DD.Systems.Room
         /// <returns></returns>
         public void CloseDoor()
         {
-            StartCoroutine(CloseDoorDynamic());
+            runningCoroutine = StartCoroutine(CloseDoorDynamic());
         }
 
         /// <summary>
