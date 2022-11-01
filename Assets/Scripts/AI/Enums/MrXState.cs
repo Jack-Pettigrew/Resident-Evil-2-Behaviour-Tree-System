7a3298dde87b2c2304cdf86c567c51105d09e2ea
Jack-Pettigrew <jak.pettigrew@gmail.com>
2022-11-01 21:02:43 +0000

    Mr X State enum

---
+++ Assets/Scripts/AI/Enums/MrXState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DD.AI.States
+{   
+    public enum MrXState
+    {
+        NONE,
+        ATTACKING,
+        SEARCHING
+    }
+}
