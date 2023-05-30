388ea259b94fe567dd66f0b2a4088dff33eeaeef
Jack-Pettigrew <jak.pettigrew@gmail.com>
2023-05-30 21:55:24 +0100

    Toggle Cursor

--- Assets/Scripts/Controller/InputManager.cs
+++ Assets/Scripts/Controller/InputManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DD.Core.Combat;
-using DD.Animation;
 
 namespace DD.Core.Control
 {
@@ -17,6 +16,9 @@ namespace DD.Core.Control
         public Vector3 InputDirection { private set; get; }
 
         [Header("Keys")]
+        public KeyCode pauseKey;
+        public event Action OnPause;
+
         public KeyCode sprintKeyCode;
         public bool Sprint { private set; get; }
 
@@ -42,39 +44,55 @@ namespace DD.Core.Control
             Instance = this;
         }
 
+#if UNITY_EDITOR
+        private void Start()
+        {
+            OnPause += CursorToggle;
+            OnInventoryToggle += CursorToggle;
+        }
+#endif
+
         private void Update()
         {
+            if (Input.GetKeyDown(pauseKey)) OnPause?.Invoke();
+
             // Global
-            if(ignoreInput) return;
+            if (ignoreInput) return;
 
             // Locomotion
-            InputDirection =  ignoreInput ? Vector3.zero : new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-            if(Sprint && InputDirection.sqrMagnitude <= 0 || Aim)
+            InputDirection = ignoreInput ? Vector3.zero : new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+            if (Sprint && InputDirection.sqrMagnitude <= 0 || Aim)
             {
                 Sprint = false;
             }
-            else if(!Aim && InputDirection.sqrMagnitude > 0 && !Sprint)
+            else if (!Aim && InputDirection.sqrMagnitude > 0 && !Sprint)
             {
                 Sprint = Input.GetKeyDown(sprintKeyCode);
             }
 
             // Combat
             Aim = Input.GetKey(aimKeyCode);
-            if(Input.GetKeyDown(aimKeyCode)) OnAimDown?.Invoke();
-            if(Input.GetKeyUp(aimKeyCode)) OnAimUp?.Invoke();
-            if(Input.GetKeyDown(shootKeyCode)) OnShoot?.Invoke();
-            if(Input.GetKeyDown(reloadKeyCode)) OnReload?.Invoke();
+            if (Input.GetKeyDown(aimKeyCode)) OnAimDown?.Invoke();
+            if (Input.GetKeyUp(aimKeyCode)) OnAimUp?.Invoke();
+            if (Input.GetKeyDown(shootKeyCode)) OnShoot?.Invoke();
+            if (Input.GetKeyDown(reloadKeyCode)) OnReload?.Invoke();
 
             // Inventory
-            if(Input.GetKeyDown(inventoryKeyCode)) OnInventoryToggle?.Invoke();
+            if (Input.GetKeyDown(inventoryKeyCode)) OnInventoryToggle?.Invoke();
 
             // Quick Slots
-            if(Input.GetKeyDown(quickSlotOneKey)) OnQuickSlotChange?.Invoke(WeaponSlot.One);
-            if(Input.GetKeyDown(quickSlotTwoKey)) OnQuickSlotChange?.Invoke(WeaponSlot.Two);
-            if(Input.GetKeyDown(quickSlotThreeKey)) OnQuickSlotChange?.Invoke(WeaponSlot.Three);
-            if(Input.GetKeyDown(quickSlotFourKey)) OnQuickSlotChange?.Invoke(WeaponSlot.Four);
+            if (Input.GetKeyDown(quickSlotOneKey)) OnQuickSlotChange?.Invoke(WeaponSlot.One);
+            if (Input.GetKeyDown(quickSlotTwoKey)) OnQuickSlotChange?.Invoke(WeaponSlot.Two);
+            if (Input.GetKeyDown(quickSlotThreeKey)) OnQuickSlotChange?.Invoke(WeaponSlot.Three);
+            if (Input.GetKeyDown(quickSlotFourKey)) OnQuickSlotChange?.Invoke(WeaponSlot.Four);
         }
 
         public void ToggleIgnoreInput(bool toggle) => ignoreInput = toggle;
+
+        public void CursorToggle()
+        {
+            Cursor.visible = !Cursor.visible;
+            Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
+        }
     }
 }
