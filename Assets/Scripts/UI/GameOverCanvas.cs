527c3e635915c2f6af02259649cf880c344bc83b
Jack-Pettigrew <jak.pettigrew@gmail.com>
2023-06-16 18:48:03 +0100

    Game Over Canvas

---
+++ Assets/Scripts/UI/GameOverCanvas.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DD.Core.Control;
+
+namespace DD.UI
+{
+    public class GameOverCanvas : MonoBehaviour
+    {
+        [field: Header("Fade Transition")]
+        [field: SerializeField, ReadOnly] public bool isTransitioning { private set; get; } = false;
+        public float fadeTime = 1.0f;
+        [SerializeField] protected CanvasGroup fadingCavasGroup;
+
+        // COROUTINE
+        protected Coroutine fadeCoroutine;
+
+        public void ToggleUI(bool toggle)
+        {
+            if(toggle)
+            {
+                StartCoroutine(FadeCorountine(0, 1, fadeTime));
+                InputManager.Instance.CursorToggle();
+            }
+            else
+            {
+                StartCoroutine(FadeCorountine(1, 0, fadeTime));
+                InputManager.Instance.CursorToggle();
+            }
+        }
+
+        protected virtual IEnumerator FadeCorountine(float startValue, float endValue, float fadeLength)
+        {
+            if(isTransitioning)
+            {
+                StopCoroutine(fadeCoroutine);
+                startValue = fadingCavasGroup.alpha;
+            }
+
+            isTransitioning = true;
+            
+            startValue = Mathf.Clamp01(startValue);
+            endValue = Mathf.Clamp01(endValue);
+
+            fadingCavasGroup.alpha = startValue;
+
+            while(fadingCavasGroup.alpha != endValue)
+            {
+                // Fade Direction
+                if(endValue > startValue)
+                {
+                    fadingCavasGroup.alpha += (1 / fadeTime) * Time.deltaTime;
+                }
+                else
+                {
+                    fadingCavasGroup.alpha -= (1 / fadeTime) * Time.deltaTime;
+                }
+                
+                yield return null;
+            }
+
+            isTransitioning = false;
+        }        
+    }
+}
