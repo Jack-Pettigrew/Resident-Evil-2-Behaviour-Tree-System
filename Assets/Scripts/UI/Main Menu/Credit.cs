ec4bb33f7cd814113489c125de21674747e1953a
Jack-Pettigrew <jak.pettigrew@gmail.com>
2023-07-25 20:15:39 +0100

    Credit Menu: Added Section Title + Full Credit List + improvements

--- Assets/Scripts/UI/Main Menu/Credit.cs
+++ Assets/Scripts/UI/Main Menu/Credit.cs
@@ -12,6 +12,8 @@ public class Credit : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     [SerializeField] private Image backgroundPanel;
     [SerializeField] private Color defaultColour;
     [SerializeField] private Color hoverColour;
+
+    private bool hasLink = false;
     
     public void SetTitle(string text)
     {
@@ -23,13 +25,42 @@ public class Credit : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         author.text = text;
     }
 
+    public void SetLink(string text)
+    {
+        if(text == string.Empty)
+        {
+            hasLink = false;
+            if (TryGetComponent<ClickableUI>(out ClickableUI clickableUI))
+            {
+                clickableUI.OnClick.RemoveAllListeners();
+            }
+        }        
+        else if (TryGetComponent<ClickableUI>(out ClickableUI clickableUI))
+        {
+            clickableUI.OnClick.AddListener(() => URLOpener.OpenURL(text));
+            hasLink = true;
+        }
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         backgroundPanel.color = hoverColour;
+
+        if(hasLink)
+        {
+            title.fontStyle = FontStyles.Underline;
+            author.fontStyle = FontStyles.Underline;
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         backgroundPanel.color = defaultColour;
+
+        if(hasLink)
+        {
+            title.fontStyle = FontStyles.Normal;
+            author.fontStyle = FontStyles.Normal;
+        }
     }
 }
--- Assets/Scripts/UI/Main Menu/CreditsCreator.cs
+++ Assets/Scripts/UI/Main Menu/CreditsCreator.cs
@@ -2,58 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using System.IO;
+using TMPro;
 
 public class CreditsCreator : MonoBehaviour
 {
     [SerializeField] private string relativeFilePath;
     public string FilePath { get { return Application.dataPath + relativeFilePath; } }
 
+    [SerializeField] private GameObject creditSectionTitlePrefab;
     [SerializeField] private GameObject creditPrefab;
     [SerializeField] private RectTransform creditsParent;
 
-    private List<Credit> creditList = new();
+    private List<GameObject> creditList = new();
 
     [ContextMenu("Test Credits")]
     public void Test()
     {
-        if (File.Exists(FilePath))
-        {
-            IEnumerable<string> lines = File.ReadLines(FilePath);
-
-            foreach (var line in lines)
-            {
-                string[] splitLine = line.Split('(', ')', '[', ']');
-                List<string> result = new();
-
-                foreach (var item in splitLine)
-                {
-                    if (item.Trim() != string.Empty)
-                    {
-                        result.Add(item);
-                    }
-                }
-
-                if (result.Count == 3)
-                {
-                    Credit credit = Instantiate(creditPrefab, Vector3.zero, Quaternion.identity, creditsParent).GetComponent<Credit>();
-                    credit.SetTitle(result[0]);
-                    credit.SetAuthor(result[1]);
-
-                    if (credit.TryGetComponent<ClickableUI>(out ClickableUI clickableUI))
-                    {
-                        clickableUI.OnClick.AddListener(() => URLOpener.OpenURL(result[2]));
-                    }
-
-                    creditList.Add(credit);
-                }
-            }
-        }
-        else
-        {
-            Debug.Log("Nope that's wrong chief");
-        }
+        UpdateCredits();
     }
 
     public void UpdateCredits()
@@ -75,18 +41,30 @@ public class CreditsCreator : MonoBehaviour
                     }
                 }
 
-                if (result.Count == 3)
+                switch (result.Count)
                 {
-                    Credit credit = Instantiate(creditPrefab, Vector3.zero, Quaternion.identity, creditsParent).GetComponent<Credit>();
-                    credit.SetTitle(result[0]);
-                    credit.SetAuthor(result[1]);
+                    // Title
+                    case 1:
+                        if(result[0][0] == '#')
+                        {
+                            TextMeshProUGUI sectionTitle = Instantiate(creditSectionTitlePrefab, Vector3.zero, Quaternion.identity, creditsParent).GetComponent<TextMeshProUGUI>();
+                            sectionTitle.text = result[0].Substring(1, result[0].Length - 1);
+                            creditList.Add(sectionTitle.gameObject);
+                        }
+                        break;
 
-                    if (credit.TryGetComponent<ClickableUI>(out ClickableUI clickableUI))
-                    {
-                        clickableUI.OnClick.AddListener(() => URLOpener.OpenURL(result[2]));
-                    }
-
-                    creditList.Add(credit);
+                    // Credit
+                    case 3:
+                        Credit credit = Instantiate(creditPrefab, Vector3.zero, Quaternion.identity, creditsParent).GetComponent<Credit>();
+                        credit.SetTitle(result[0]);
+                        credit.SetAuthor(result[1]);
+                        credit.SetLink(result[2]);
+                        
+                        creditList.Add(credit.gameObject);
+                        break;
+                        
+                    default:
+                        break;
                 }
             }
         }
@@ -98,9 +76,9 @@ public class CreditsCreator : MonoBehaviour
 
     public void RemoveCredits()
     {
-        foreach (Credit item in creditList)
+        foreach (GameObject item in creditList)
         {
-            Destroy(item.gameObject);
+            Destroy(item);
         }
 
         creditList.Clear();
