0ac174caaf884cbc7cd4cec1e8722ba91912a4c9
Jack-Pettigrew <jak.pettigrew@gmail.com>
2023-08-03 21:19:44 +0100

    Interactor Updates - tracks nearest interactables solo + new events

--- Assets/Scripts/Controller/Interactors/Interactor.cs
+++ Assets/Scripts/Controller/Interactors/Interactor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,117 +12,105 @@ namespace DD.Core.Control
         [SerializeField] private float range = 1.0f;
         [SerializeField] private float maxAngle = 90.0f;
 
+        // [SerializeField] private float nearestInteractablesPolling = 0.2f;
+
+        // Interactable-GameObject Tuple
+        private (IInteractable interactable, GameObject interactableGameObject) nearestTarget = (null, null);
+
         // Interactor Info
         public Transform InteractorTransform { private set; get; }
         [SerializeField] private InteractorType interactorType;
         public InteractorType InteractorType { get { return interactorType; } }
-        
-        private void Awake() 
+
+        // Coroutines
+        private Coroutine coroutine = null;
+
+        // Event
+        public Action<IInteractable, GameObject> OnNearestInteractableFound;
+        public Action OnNearestInteractableLost;
+
+        private void Awake()
         {
             InteractorTransform = transform;
         }
-        
-        private void Update() 
-        {
-            if(Input.GetKeyDown(KeyCode.F))
+
+        private void Start() {
+            coroutine = StartCoroutine(FindNearestInteractable());
+        }
+
+        private void Update()
+        {   
+            if (Input.GetKeyDown(KeyCode.F))
             {
                 Interact();
             }
         }
-        
+
         private void Interact()
         {
-            IInteractable interactable = FindNearestInteractable(out GameObject interactableGameObject);
-
             // Need to call event before interacting as depending in the interactable's logic it could be destroyed
-            
-            if(interactableGameObject)
+
+            if (nearestTarget != (null, null))
             {
-                GlobalEvents.OnInteract?.Invoke(interactableGameObject);
+                GlobalEvents.OnInteract?.Invoke(nearestTarget.interactableGameObject);
             }
-            
-            interactable?.Interact(this);
+
+            nearestTarget.interactable?.Interact(this);
         }
 
-        /// <summary>
-        /// Returns the nearest interactable.
-        /// </summary>
-        /// <returns>The nearest interactable. Null if none found</returns>
-        private IInteractable FindNearestInteractable()
+        private IEnumerator FindNearestInteractable()
         {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, range);
-            IInteractable interactable = null;
-            float closestDist = range;
-
-            foreach (Collider collider in colliders)
+            while (true)
             {
-                IInteractable foundInteractable = collider.GetComponent<IInteractable>();
+                Collider[] colliders = Physics.OverlapSphere(transform.position, range);
+                float closestDist = range;
+                IInteractable nearestInteractable = null;
+                GameObject nearestInteractableGameobject = null;
 
-                // This is a child object, get from parent
-                if(foundInteractable == null)
+                foreach (Collider collider in colliders)
                 {
-                    foundInteractable = collider.GetComponentInParent<IInteractable>();
-                }
-                
-                if(foundInteractable != null && foundInteractable.CanInteract)
-                {                    
-                    Vector3 closestPoint = collider.ClosestPoint(transform.position);
-                    float angle = Vector3.Angle(transform.forward, closestPoint - transform.position);
-                    float dist = Vector3.Distance(transform.position, closestPoint);
+                    IInteractable foundInteractable = collider.GetComponent<IInteractable>();
 
-                    if(angle < maxAngle && dist < closestDist)
+                    // This is a child object, get from parent
+                    if (foundInteractable == null)
                     {
-                        closestDist = dist; 
-                        interactable = foundInteractable;
+                        foundInteractable = collider.GetComponentInParent<IInteractable>();
+                    }
+
+                    if (foundInteractable != null && foundInteractable.CanInteract)
+                    {
+                        Vector3 closestPoint = collider.ClosestPoint(transform.position);
+                        float angle = Vector3.Angle(transform.forward, closestPoint - transform.position);
+                        float dist = Vector3.Distance(transform.position, closestPoint);
+
+                        if (angle < maxAngle && dist < closestDist)
+                        {
+                            closestDist = dist;
+
+                            nearestInteractable = foundInteractable;
+                            nearestInteractableGameobject = collider.gameObject;
+                        }
                     }
                 }
-            }
 
-            return interactable;
-        }
-
-        /// <summary>
-        /// Returns the nearest interactable.
-        /// </summary>
-        /// <param name="gameObject">The GameObject associated with this interactable. Null if none found.</param>
-        /// <returns>The nearest interactable. Null if none found.</returns>
-        private IInteractable FindNearestInteractable(out GameObject gameObject)
-        {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, range);
-            IInteractable interactable = null;
-            gameObject = null;
-            float closestDist = range;
-
-            foreach (Collider collider in colliders)
-            {
-                IInteractable foundInteractable = collider.GetComponent<IInteractable>();
-
-                // This is a child object, get from parent
-                if(foundInteractable == null)
+                // Found 
+                if(nearestInteractable != null && nearestInteractableGameobject != null && nearestTarget != (nearestInteractable, nearestInteractableGameobject))
                 {
-                    foundInteractable = collider.GetComponentInParent<IInteractable>();
+                    nearestTarget = (nearestInteractable, nearestInteractableGameobject);
+                    OnNearestInteractableFound?.Invoke(nearestInteractable, nearestInteractableGameobject);
                 }
-                
-                if(foundInteractable != null && foundInteractable.CanInteract)
-                {                    
-                    Vector3 closestPoint = collider.ClosestPoint(transform.position);
-                    float angle = Vector3.Angle(transform.forward, closestPoint - transform.position);
-                    float dist = Vector3.Distance(transform.position, closestPoint);
+                else if(nearestInteractable == null && nearestInteractableGameobject == null && nearestTarget != (null, null) ) {
+                    nearestTarget = (null, null);
+                    OnNearestInteractableLost?.Invoke();
+                }
 
-                    if(angle < maxAngle && dist < closestDist)
-                    {
-                        closestDist = dist; 
-                        interactable = foundInteractable;
-                        gameObject = collider.gameObject;
-                    }
-                }
+                yield return null;
+                // yield return new WaitForSeconds(nearestInteractablesPolling);
             }
-
-            return interactable;
         }
 
-
-        private void OnDrawGizmosSelected() {
+        private void OnDrawGizmosSelected()
+        {
             Gizmos.color = Color.green;
             Gizmos.DrawWireSphere(transform.position, range);
             Gizmos.DrawLine(transform.position, transform.position + Quaternion.Euler(0, maxAngle, 0) * (transform.forward * range));
