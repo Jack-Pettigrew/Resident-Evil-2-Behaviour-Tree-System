8b87b85faf57aff023839c1f427c9474e157ed1b
Jack-Pettigrew <jak.pettigrew@gmail.com>
2023-08-17 21:26:23 +0100

    Player Footsteps

--- Assets/Scripts/Sounds/FootstepAudioSource.cs
+++ Assets/Scripts/Sounds/FootstepAudioSource.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class FootstepAudioSource : MonoBehaviour
 {
+    [SerializeField] private Transform comparisonTransform;
+
     private float currentPitch = 1.0f;
     [SerializeField, Range(0f, 1.0f)] private float minRandomPitch, maxRandomPitch;
     
     [SerializeField] private AudioClip footstepSound;
     [SerializeField] private AudioSource leftFoot, rightFoot;
-    
+
+    private void LateUpdate()
+    {
+        if (Mathf.Abs((comparisonTransform.position - leftFoot.transform.position).y) <= 0.15f)
+        {
+            PlayFootstep(Foot.LEFT);
+        }
+
+        if (Mathf.Abs((comparisonTransform.position - rightFoot.transform.position).y) <= 0.15f)
+        {
+            PlayFootstep(Foot.RIGHT);
+        }
+    }
+
     public void PlayFootstep(Foot foot)
     {
         AudioSource audioSource = foot == Foot.LEFT ? leftFoot : rightFoot;
@@ -25,6 +40,12 @@ public class FootstepAudioSource : MonoBehaviour
 
         // Debug.Log(foot.ToString());
     }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawLine(leftFoot.transform.position, comparisonTransform.position);
+    //}
 }
 
 public enum Foot {
