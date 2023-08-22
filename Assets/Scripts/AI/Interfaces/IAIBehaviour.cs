ba085a6fb6da470f3ec621cb00aa4f1c1739efe2
Jack-Pettigrew <jak.pettigrew@gmail.com>
2023-08-22 10:39:22 +0100

    Moved AI interfaces directories

--- Assets/Scripts/AI/Interfaces/IAIBehaviour.cs
+++ Assets/Scripts/AI/Interfaces/IAIBehaviour.cs
@@ -4,8 +4,6 @@ using DD.Core.Control;
 
 namespace DD.AI.Controllers
 {
-    // LISKOV: This interface forces all AI controllers to implement these functions even if the BT doesn't necessarily need them... not sure how to get around this currently
-
     /// <summary>
     /// The AI Interface for AI variables and logic decoupled from the Behaviour Tree.
     /// </summary>
