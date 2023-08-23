231f063f702e108d77897fd535e6885c300f35cc
Jack-Pettigrew <jak.pettigrew@gmail.com>
2023-08-23 18:13:59 +0100

    ItemData inheritance fix

--- Assets/Scripts/Scriptable Objects/Items/AmmoItem.cs
+++ Assets/Scripts/Scriptable Objects/Items/AmmoItem.cs
@@ -6,8 +6,11 @@ using DD.Core.Combat;
 namespace DD.Core.Items
 {
     [CreateAssetMenu(menuName = "Items/Ammo Item", fileName = "AmmoItem", order = 3)]
-    public class AmmoItem : EquipmentItem
-    {   
+    public class AmmoItem : ItemData
+    {
+        [field: Header("Equipment")]
+        [field: SerializeField] public EquipmentType EquipmentType { private set; get; }
+
         public override void Use()
         {
             // Send to ItemCombiner to combine with weapon/ammo logic
